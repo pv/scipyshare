94c776149756b7a9bcbf85ea9fff43f0f485d040
Pauli Virtanen <pav@iki.fi>
2011-03-05 18:54:12 +0100

    ENH: community: add tag categories

--- scipyshare/community/models.py
+++ scipyshare/community/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 from scipyshare.catalog.models import Entry
 
+class TagCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField()
+
 class Tag(models.Model):
     """
     Free-form tags and trove classifiers
@@ -13,6 +17,7 @@ class Tag(models.Model):
     name = models.SlugField(unique=True,
                             help_text="name *and* slug for the tag")
     description = models.TextField(help_text="description for the tag")
+    category = models.ForeignKey(TagCategory)
 
     entries = models.ManyToManyField(Entry, related_name="tags")
 
