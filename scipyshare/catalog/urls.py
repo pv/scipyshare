4d5624ca83309def0b81d6a55f62ffe6e92ec18c
Pauli Virtanen <pav@iki.fi>
2011-04-04 22:35:21 +0200

    BUG: catalog: fix wrong slug characters in catalog urlconf

--- scipyshare/catalog/urls.py
+++ scipyshare/catalog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('scipyshare.catalog.views',
     (r'^$', 'index'),
     (r'^new/$', 'new_entry'),
-    (r'^(?P<slug>[a-z0-9-]+)/$', 'view'),
-    (r'^(?P<slug>[a-z0-9-]+)/edit/$', 'edit_entry'),
-    (r'^(?P<slug>[a-z0-9-]+)/file/(?P<file_name>.+)/$', 'download'),
+    (r'^(?P<slug>[\w_.-]+)/$', 'view'),
+    (r'^(?P<slug>[\w_.-]+)/edit/$', 'edit_entry'),
+    (r'^(?P<slug>[\w_.-]+)/file/(?P<file_name>.+)/$', 'download'),
 )
